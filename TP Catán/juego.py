f8738316a69a39dbc9efb70ae2597b9ebc2ec9e9
Matias Arazi <71347020+MatiArazi@users.noreply.github.com>
2022-09-07 12:10:00 -0300

    3.3

--- TP Catán/juego.py
+++ TP Catán/juego.py
@@ -1,23 +1,31 @@
+import random
 ORDEN_ESPECIAL = False
 
 def tirar_dados():
     pass
 
 def rellenar_tablero(tablero):
-    recurso = ["Ladrillo"] * 3 + ["Piedra"] * 3 + ["Trigo"] * 4 + ["Lane"] * 4 + ["Madera"] * 4
-    numeross.push(2)
+    recurso = ["Ladrillo"] * 3 + ["Piedra"] * 3 + ["Trigo"] * 4 + ["Lana"] * 4 + ["Madera"] * 4
+    numeross = []
+    numeross.append(2)
     for i in range(3,12):
         if i == 7:
-            pass
-        numeross.push(i)
-        numeross.push(i)
-    numeross.push(12)
-
-    for i in range(numeross.size()):
-        rand = randint(0,numeross.size())
-        colocar_recurso_y_numero(numeross[rand], recurso[rand])
-        recurso.delete(rand)
-        numero.delete(rand)
+            continue
+        numeross.append(i)
+        numeross.append(i)
+    numeross.append(12)
+    ficha = 1
+    for i in range(len(numeross)):
+        rand = random.randint(0,len(numeross)-1)
+        print(rand)
+        print(numeross)
+        print(recurso)
+        tablero.colocar_recurso_y_numero(ficha,recurso[rand], numeross[rand])
+        ficha+=1
+        if ficha == 10:
+            ficha+=1
+        recurso.pop(rand)
+        numeross.pop(rand)
 
 
 def jugar_catan(jugadores,tablero):
